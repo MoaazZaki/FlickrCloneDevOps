13a7bb95f487347e2aa95f35e0ce4786552ae990
MoaazZaki <moaazzaki1999@gmail.com>
2021-05-23 20:57:38 +0200

    Edit Dockerfile with extra effiecint build method

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@ sudo apt update
 sudo apt install jq
 sudo snap install docker 
 sudo apt install nginx
+sudo apt-get install yarn
 sudo docker login -u <user> -p <password>
 git clone https://<user>:<password>@github.com/MoaazZaki/jdc-trial.git
 cd /etc/nginx/sites-enabled
@@ -12,5 +13,10 @@ cd
 sudo nginx -s reload
 sudo docker run -d --rm --name dd-agent -v /var/run/docker.sock:/var/run/docker.sock:ro -v /proc/:/host/proc/:ro -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro -e DD_API_KEY=b5ab94713bb50f242a6b43aa21b2f4fa -e DD_SITE="datadoghq.com" gcr.io/datadoghq/agent:7
 cd jdc-trial
+
+sudo mv client Flickr-Frontend
+sudo mv server FlickrCloneBE
+
+
 sh auto_update.sh
 sudo docker update --restart unless-stopped $(sudo docker ps -q)
