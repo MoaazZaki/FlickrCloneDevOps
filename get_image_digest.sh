250f587ce1b8494ff5f43e4a179de45f7dde0404
MoaazZaki <moaazzaki1999@gmail.com>
2021-05-28 00:24:39 +0200

    FIxed automation scrpit

--- get_image_digest.sh
+++ get_image_digest.sh
@@ -5,10 +5,8 @@ TARGET_TAG=$2
 TOKEN=$(curl -s "https://auth.docker.io/token?service=registry.docker.io&scope=repository:$REPOSITORY:pull" | jq -r .token )
 # get image digest for target
 TARGET_DIGEST=$(curl -s -D - -H "Authorization: Bearer $TOKEN" -H "Accept: application/vnd.docker.distribution.manifest.v2+json" https://index.docker.io/v2/$REPOSITORY/manifests/$TARGET_TAG | grep docker-content-digest | cut -d ' ' -f 2 | head -n 1 | tr -d '\r')
-echo $TARGET_DIGEST
 # pulled iamge digest
 CURRENT_DIGEST=$(sudo docker image inspect "$REPOSITORY:$TARGET_TAG" --format '{{json .RepoDigests}}' | jq . | grep $REPOSITORY | sed 's/.$//' | sed 's/^.*\(sha.*\).*$/\1/') 
-echo $CURRENT_DIGEST
 # check digest
   if [ "$TARGET_DIGEST" = "$CURRENT_DIGEST" ]; then
     echo "true"
